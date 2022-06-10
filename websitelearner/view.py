d20a98df691811c6998965bdec4ac95d99e12717
Akash B <akashbadole@rediffmail.com>
2022-06-10 10:01:54 +0530

    themewagon theme integrated to Django

--- websitelearner/view.py
+++ websitelearner/view.py
@@ -2,21 +2,22 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    data={
-        'title':'Data transfer - home page title',
-        'data': 'Learning django',
-        'clist': ['php', 'java', 'python'],
-        'numbers':[30,20,16,45,65,26,60,19,90,57,71,37,82],
-        'student_details':[
-            {'name':'Jai', 'phone':353433544343},
-            {'name':'Rai', 'phone':4653543},
+    # data={
+    #     'title':'Data transfer - home page title',
+    #     'data': 'Learning django',
+    #     'clist': ['php', 'java', 'python'],
+    #     'numbers':[30,20,16,45,65,26,60,19,90,57,71,37,82],
+    #     'student_details':[
+    #         {'name':'Jai', 'phone':353433544343},
+    #         {'name':'Rai', 'phone':4653543},
 
-        ]
-    }
-    return render(request, 'index.html', data)
+    #     ]
+    # }
+    # return render(request, 'index.html', data)
+    return render(request, 'index.html')
 
 def blog(request, courseid):
     return HttpResponse(courseid)
 
 def homepage(request):
-    return HttpResponse("hi, I love djnago lang")
+    return HttpResponse("hi, I love django lang")
